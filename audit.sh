06f91227e161b1a76d95658c3806d640785c594b
developer <developer@ghost.0.2.15>
2022-01-11 05:22:22 -0500

    mythril over all files in folder

--- audit.sh
+++ audit.sh
@@ -4,7 +4,7 @@ echo ''
 echo 'Mythril analysis'
 for file in $(find contracts/ -name '*.sol')
 do 
-	echo 'auditing of ' $file
-	docker run -v $(pwd):/tmp --rm mythril/myth analyze /tmp/contracts/eDIVI.sol --solv 0.8.7
+	echo 'auditing of '$file
+	docker run -v $(pwd):/tmp --rm mythril/myth analyze /tmp/$file --solv 0.8.7
 	echo ''
 done
